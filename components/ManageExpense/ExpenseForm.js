d58b9c3ffc838001b93cb99c0d9bba69c1596976
peter_alfie <alfie1104@gmail.com>
2022-06-28 10:51:17 +0900

    feat(expenseform): minor modification

--- components/ManageExpense/ExpenseForm.js
+++ components/ManageExpense/ExpenseForm.js
@@ -6,29 +6,31 @@ function ExpenseForm() {
 
   return (
     <View>
-      <Input
-        label="Amout"
-        textInputConfig={{
-          keyboardType: "decimal-pad",
-          onChangeText: amountChangeHandler,
-        }}
-      />
-      <Input
-        label="Date"
-        textInputConfig={{
-          placehoder: "YYYY-MM-DD",
-          maxLength: 10,
-          onChangeText: () => {},
-        }}
-      />
-      <Input
-        label="Description"
-        textInputConfig={{
-          multiline: true,
-          //autoCapitalize: 'none'
-          //autoCorrect: false // default is true
-        }}
-      />
+      <View>
+        <Input
+          label="Amout"
+          textInputConfig={{
+            keyboardType: "decimal-pad",
+            onChangeText: amountChangeHandler,
+          }}
+        />
+        <Input
+          label="Date"
+          textInputConfig={{
+            placeholder: "YYYY-MM-DD",
+            maxLength: 10,
+            onChangeText: () => {},
+          }}
+        />
+        <Input
+          label="Description"
+          textInputConfig={{
+            multiline: true,
+            //autoCapitalize: 'none'
+            //autoCorrect: false // default is true
+          }}
+        />
+      </View>
     </View>
   );
 }
