772bf6bb0a8ac73f9e1975ffcbe55d1f52629047
peter_alfie <alfie1104@gmail.com>
2022-06-29 08:17:43 +0900

    feat(expenseform, manageexpense): working with Entered Data

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -26,19 +26,11 @@ function ManageExpense({ route, navigation }) {
     navigation.goBack();
   }
 
-  function confirmHandler() {
+  function confirmHandler(expenseData) {
     if (isEditing) {
-      expensesCtx.updateExpense(editedExpenseId, {
-        description: "Test!!!",
-        amount: 29.99,
-        date: new Date("2022-06-27"),
-      });
+      expensesCtx.updateExpense(editedExpenseId, expenseData);
     } else {
-      expensesCtx.addExpense({
-        description: "Test",
-        amount: 19.99,
-        date: new Date("2022-06-26"),
-      });
+      expensesCtx.addExpense(expenseData);
     }
     navigation.goBack();
   }
