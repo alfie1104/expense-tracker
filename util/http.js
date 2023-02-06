9e656be1b8eaa0f169ef32eb886d07a10b8009b3
peter_alfie <alfie1104@gmail.com>
2023-02-06 22:12:37 +0900

    feat(expenses-context, recentexpenses): fetching backend data and using the data with transforming

--- util/http.js
+++ util/http.js
@@ -2,6 +2,23 @@ import axios from "axios";
 import { API_URL } from "@env";
 
 export function storeExpense(expenseData) {
-  console.log(API_URL);
   axios.post(`${API_URL}/expenses.json`, expenseData);
+}
+
+export async function fetchExpenses() {
+  const response = await axios.get(`${API_URL}/expenses.json`);
+
+  const expenses = [];
+
+  for (const key in response.data) {
+    const expenseObj = {
+      id: key,
+      amount: response.data[key].amount,
+      date: new Date(response.data[key].date),
+      description: response.data[key].description,
+    };
+    expenses.push(expenseObj);
+  }
+
+  return expenses;
 }
